e1d0274def1639fca4f472dc833fd87da85fa138
WaleedAhmed05 <waloo_waleed@yahoo.com>
2023-06-23 01:15:38 -0400

    Creating separate process for each customer/bank

--- money.erl
+++ money.erl
@@ -13,4 +13,34 @@ start(Args) ->
 
 
 
+    %print_tuple(CustomerInfo).
+    % io:format("Type of Variable: ~p~n", [CustomerInfo]).
+    %start_new_process().
+    startX(CustomerInfo).
+
+
+    startX(Tuples) ->
+        PidList = spawn_processes(Tuples, []),
+        io:format("Process Pids: ~p~n", [PidList]).
+    
+    spawn_processes([], PidList) ->
+        lists:reverse(PidList);
+    spawn_processes([{Value1, Value2} | Rest], PidList) ->
+        Pid = spawn(fun() -> process_tuple(Value1, Value2) end),
+        spawn_processes(Rest, [Pid | PidList]).
+    
+    process_tuple(Value1, Value2) ->
+        % Perform some processing with the tuple values
+        io:format("Processing tuple: ~p, ~p~n", [Value1, Value2]).
+
+
+%To print file content.
+% print_tuples([]) ->  % Base case: empty list
+%     ok;
+% print_tuples([Tuple | Tuples]) ->
+%     print_tuple(Tuple),
+%     print_tuples(Tuples).
+
+% print_tuple(Tuple) ->
+%     io:format("~p~n", [Tuple]).
 
